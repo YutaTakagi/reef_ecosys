45c75ab9b3beffe876a0b8e862e320431d80350a
Jorge Suarez <116789662+georgesuac@users.noreply.github.com>
2024-05-17 15:35:13 +0900

    Phyt4 added as a new component (iCyan)

--- Projects/foodweb/run.sh
+++ Projects/foodweb/run.sh
@@ -4,10 +4,11 @@ rm *.exe
 #
 SRC_DIR=../../src
 INCLUDE="-I${PWD}"
-gfortran -fbounds-check -ffree-form ${SRC_DIR}/mod_calendar.f90 ${SRC_DIR}/mod_geochem.F ${SRC_DIR}/mod_reef_flow.F ${SRC_DIR}/mod_heat.F ${SRC_DIR}/mod_foodweb.F ${SRC_DIR}/mod_param.F ${SRC_DIR}/mod_coral.F ${SRC_DIR}/mod_macroalgae.F ${SRC_DIR}/mod_seagrass.F ${SRC_DIR}/mod_sedecosys.F ${SRC_DIR}/mod_reef_ecosys.F ${SRC_DIR}/mod_input.F ${SRC_DIR}/mod_output.F ${SRC_DIR}/main.F -O2 ${INCLUDE} -I/usr/include -L/usr/lib -lnetcdff -o ecosys_test.exe
+gfortran -fbounds-check -ffree-form ${SRC_DIR}/mod_calendar.f90 ${SRC_DIR}/mod_geochem.F ${SRC_DIR}/mod_reef_flow.F ${SRC_DIR}/mod_heat.F ${SRC_DIR}/mod_foodweb.F ${SRC_DIR}/mod_param.F ${SRC_DIR}/mod_coral.F ${SRC_DIR}/mod_macroalgae.F ${SRC_DIR}/mod_seagrass.F ${SRC_DIR}/mod_sedecosys.F ${SRC_DIR}/mod_reef_ecosys.F ${SRC_DIR}/mod_input.F ${SRC_DIR}/mod_output.F ${SRC_DIR}/main.F -O2 ${INCLUDE} -I/opt/homebrew/include -L/opt/homebrew/lib -lnetcdff -o ecosys_test.exe
 rm *.mod
 #
 mkdir -p output
 #
-./ecosys_test.exe < foodweb.in
+./ecosys_test.exe < foodweb2022control.in
+#./ecosys_test.exe < foodweb2023.in
 #
