15533b4aa639ed4d1672c03a9c5eff97e8a669e4
Takashi NAKAMURA <nakamura.t.av@m.titech.ac.jp>
2025-01-03 23:43:18 +0900

    Added mod_reef_ecosys_param.F and mod_decomposition.F, and related modification

--- Projects/pelagic_bentic/run_mac.sh
+++ Projects/pelagic_bentic/run_mac.sh
@@ -4,10 +4,29 @@ rm *.exe
 #
 SRC_DIR=../../src
 INCLUDE="-I${PWD}"
-# for Windows
-#gfortran -fbounds-check -ffree-form ${SRC_DIR}/mod_calendar.f90 ${SRC_DIR}/mod_geochem.F ${SRC_DIR}/mod_reef_flow.F ${SRC_DIR}/mod_heat.F ${SRC_DIR}/mod_foodweb.F ${SRC_DIR}/mod_param.F ${SRC_DIR}/mod_coral.F ${SRC_DIR}/mod_macroalgae.F ${SRC_DIR}/mod_seagrass.F ${SRC_DIR}/mod_sedecosys.F ${SRC_DIR}/mod_reef_ecosys.F ${SRC_DIR}/mod_input.F ${SRC_DIR}/mod_output.F ${SRC_DIR}/main.F -O2 ${INCLUDE} -I/usr/include -L/usr/lib -lnetcdff -o ecosys_test.exe
-# for MacOS
-gfortran -fbounds-check -ffree-form ${SRC_DIR}/mod_calendar.f90 ${SRC_DIR}/mod_geochem.F ${SRC_DIR}/mod_reef_flow.F ${SRC_DIR}/mod_heat.F ${SRC_DIR}/mod_foodweb.F ${SRC_DIR}/mod_param.F ${SRC_DIR}/mod_coral.F ${SRC_DIR}/mod_macroalgae.F ${SRC_DIR}/mod_seagrass.F ${SRC_DIR}/mod_sedecosys.F ${SRC_DIR}/mod_reef_ecosys.F ${SRC_DIR}/mod_input.F ${SRC_DIR}/mod_output.F ${SRC_DIR}/main.F -O2 ${INCLUDE} -I/opt/homebrew/include -L/opt/homebrew/lib -lnetcdff -o ecosys_test.exe
+FFLAGS="-fbounds-check -ffree-form -O3"
+#FFLAGS="-fbounds-check -ffree-form -O0 -g -fcheck=array-temps,bounds,do,mem,pointer,recursion"
+
+gfortran ${FFLAGS} \
+  ${SRC_DIR}/mod_calendar.f90 \
+  ${SRC_DIR}/mod_geochem.F  \
+  ${SRC_DIR}/mod_reef_ecosys_param.F \
+  ${SRC_DIR}/mod_param.F \
+  ${SRC_DIR}/mod_reef_flow.F \
+  ${SRC_DIR}/mod_heat.F \
+  ${SRC_DIR}/mod_decomposition.F \
+  ${SRC_DIR}/mod_foodweb.F \
+  ${SRC_DIR}/mod_coral.F \
+  ${SRC_DIR}/mod_macroalgae.F \
+  ${SRC_DIR}/mod_seagrass.F \
+  ${SRC_DIR}/mod_sedecosys.F \
+  ${SRC_DIR}/mod_reef_ecosys.F \
+  ${SRC_DIR}/mod_input.F \
+  ${SRC_DIR}/mod_output.F \
+  ${SRC_DIR}/main.F \
+  ${INCLUDE} -I/opt/homebrew/include -L/opt/homebrew/lib -lnetcdff \
+  -o ecosys_test.exe
+
 rm *.mod
 #
 mkdir -p output
--- Projects/sedecosys/run1.sh
+++ Projects/sedecosys/run1.sh
@@ -4,15 +4,32 @@ rm *.exe
 #
 SRC_DIR=../../src
 INCLUDE="-I${PWD}"
+FFLAGS="-fbounds-check -ffree-form -O3"
+#FFLAGS="-fbounds-check -ffree-form -O0 -g -fcheck=array-temps,bounds,do,mem,pointer,recursion"
 
-#gfortran -fbounds-check -ffree-form -O0 -g -fcheck=array-temps,bounds,do,mem,pointer,recursion ${SRC_DIR}/mod_calendar.f90 ${SRC_DIR}/mod_geochem.F ${SRC_DIR}/mod_foodweb.F ${SRC_DIR}/mod_param.F ${SRC_DIR}/mod_reef_flow.F ${SRC_DIR}/mod_heat.F ${SRC_DIR}/mod_coral.F ${SRC_DIR}/mod_macroalgae.F ${SRC_DIR}/mod_seagrass.F ${SRC_DIR}/mod_sedecosys.F ${SRC_DIR}/mod_reef_ecosys.F ${SRC_DIR}/mod_input.F ${SRC_DIR}/mod_output.F ${SRC_DIR}/main.F -O2 ${INCLUDE} -I/usr/include -L/usr/lib -lnetcdff -o ecosys_test.exe
-gfortran -fbounds-check -ffree-form -O3 ${SRC_DIR}/mod_calendar.f90 ${SRC_DIR}/mod_geochem.F ${SRC_DIR}/mod_foodweb.F ${SRC_DIR}/mod_param.F ${SRC_DIR}/mod_reef_flow.F ${SRC_DIR}/mod_heat.F ${SRC_DIR}/mod_coral.F ${SRC_DIR}/mod_macroalgae.F ${SRC_DIR}/mod_seagrass.F ${SRC_DIR}/mod_sedecosys.F ${SRC_DIR}/mod_reef_ecosys.F ${SRC_DIR}/mod_input.F ${SRC_DIR}/mod_output.F ${SRC_DIR}/main.F -O2 ${INCLUDE} -I/usr/include -L/usr/lib -lnetcdff -o ecosys_test.exe
+gfortran ${FFLAGS} \
+  ${SRC_DIR}/mod_calendar.f90 \
+  ${SRC_DIR}/mod_geochem.F  \
+  ${SRC_DIR}/mod_reef_ecosys_param.F \
+  ${SRC_DIR}/mod_param.F \
+  ${SRC_DIR}/mod_reef_flow.F \
+  ${SRC_DIR}/mod_heat.F \
+  ${SRC_DIR}/mod_decomposition.F \
+  ${SRC_DIR}/mod_foodweb.F \
+  ${SRC_DIR}/mod_coral.F \
+  ${SRC_DIR}/mod_macroalgae.F \
+  ${SRC_DIR}/mod_seagrass.F \
+  ${SRC_DIR}/mod_sedecosys.F \
+  ${SRC_DIR}/mod_reef_ecosys.F \
+  ${SRC_DIR}/mod_input.F \
+  ${SRC_DIR}/mod_output.F \
+  ${SRC_DIR}/main.F \
+  ${INCLUDE} -I/usr/include -L/usr/lib -lnetcdff \
+  -o ecosys_test.exe
 
-# gfortran -fbounds-check -ffree-form ${SRC_DIR}/mod_calendar.f90 ${SRC_DIR}/mod_geochem.F ${SRC_DIR}/mod_param.F ${SRC_DIR}/mod_reef_flow.F ${SRC_DIR}/mod_heat.F ${SRC_DIR}/mod_foodweb.F ${SRC_DIR}/mod_coral.F ${SRC_DIR}/mod_macroalgae.F ${SRC_DIR}/mod_seagrass.F ${SRC_DIR}/mod_sedecosys_MM4.F ${SRC_DIR}/mod_reef_ecosys_MM1.F ${SRC_DIR}/mod_input.F ${SRC_DIR}/mod_output.F ${SRC_DIR}/main.F -O2 ${INCLUDE} -I/opt/homebrew/Cellar/netcdf-fortran/4.6.1/include -I/opt/homebrew/Cellar/netcdf-fortran/4.6.1/include -L/opt/homebrew/Cellar/netcdf-fortran/4.6.1/lib -lnetcdff -o ecosys_test.exe
 rm *.mod
 #
-# mkdir -p output
+mkdir -p output
 #
 ./ecosys_test.exe < sedecosys.in
-# ./ecosys_test.exe
 #
